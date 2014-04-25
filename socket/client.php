ccdddfab79a41579287cb3db13fa3dbf5cd7b09e
coderookie <zhanglei19881228@sina.com>
2014-04-25 09:52:13 +0800

    fix bug for socket client application

--- socket/client.php
+++ socket/client.php
@@ -1,11 +1,11 @@
 <?php
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-$connection = socket_connect($socket, 'localhost', 4888) or die("Could not connect\n");
+$connection = socket_connect($socket, 'localhost', 8888) or die("Could not connect\n");
 
 $data = 'i love adele';
 $length = strlen($data);
-if(!socket_sendto($socket, $data, $length, 0, '127.0.0.1', 4888)){
+if(!socket_sendto($socket, $data, $length, 0, '127.0.0.1', 8888)){
     echo "can not send data to the destination host";
 }
 
