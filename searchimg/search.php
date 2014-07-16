e8a6648dd72f228c5a73bd0cec92caafacfc6e9c
coderookie <zhanglei19881228@sina.com>
2014-07-16 09:59:08 +0800

    improve searchimages

--- searchimg/search.php
+++ searchimg/search.php
@@ -1,46 +1,79 @@
 <meta charset='utf8' />
 <?php
-if(file_exists('../autoload/loader.class.php')){
-    require_once('../autoload/loader.class.php');
-}else{
-    throw new Exception('loader.class.php is not exists');
-}
+if(isset($_SERVER['REQUEST_METHOD']) && strtolower($_SERVER['REQUEST_METHOD']) == 'post'){
 
-spl_autoload_register(array('loader', 'loadClass'));
+    if(file_exists('../autoload/loader.class.php')){
+        require_once('../autoload/loader.class.php');
+    }else{
+        throw new Exception('loader.class.php is not exists');
+    }
 
-$Upload = Upload::getInstance();
-$ImagesHash = ImagesHash::getInstance();
+    spl_autoload_register(array('loader', 'loadClass'));
 
-$conf = array(
-    'host' => 'localhost',
-    'user' => 'root',
-    'pass' => 'root',
-    'db'   => 'demo',
-    'port' => '3306'
-);
-$_name = 'images';
-$DBHelper = DBHelper::getInstance($conf);
+    $Upload = Upload::getInstance();
+    $ImagesHash = ImagesHash::getInstance();
 
-$act = isset($_GET['act']) ? $_GET['act'] : false;
-if($act == 'file'){
+    $conf = array(
+        'host' => 'localhost',
+        'user' => 'root',
+        'pass' => 'root',
+        'name' => 'demo',
+        'port' => '3306'
+    );
+    $_name = 'images';
+    $Db = Db::getInstance($conf);
+
+    /* 先查看有没有相似的图片, 不管有没有, 将用户上传的图片提交到数据库 */
+
     $image_info = $Upload->uploadFile('imgkeyword');
-
     if($image_info['code'] !== 0){
         throw new Exception('上传图片错误');
     }
+    $path = $image_info['path'];
+    $hash = $ImagesHash->getImagesHash($path);
     $data = array(
-        'path' => $image_info['path'],
-        'hash' => $ImagesHash->getImagesHash($image_info['path'])
+        'path' => $path,
+        'hash' => $hash
     );
-    $DBHelper->write($_name, $data);
+    
+    // 拿用户刚刚上传的图片的hash值, 去数据库匹配, 匹配到了, 返回
+    $lists = array();
+    $field = '*';
+    $total_lists = $Db->fetchAll($_name, $field);
+    if(!empty($total_lists)){
+        foreach($total_lists as $image){
+            if($ImagesHash->checkIsSimilarImg($hash, $image['hash'])){
+                $lists[] = $image;
+            }
+        }
+    }
+    
+    // 将用户上传的图片insert去数据库
+    if(!$Db->insert($_name, $data)){
+        throw new Exception('添加到数据库失败');
+    }
 
-}else{
+}
 ?>
-<form action='?act=file' method='post' enctype="multipart/form-data">
+<form action='search.php' method='post' enctype="multipart/form-data">
     上传图片: <input name='imgkeyword' type='file' />
     <br /><br />
     <input name='submit' value='上传' type='submit' />
 </form>
-<?php
-}
-?>
+
+
+<?php if(isset($_SERVER['REQUEST_METHOD']) && strtolower($_SERVER['REQUEST_METHOD']) == 'post'): ?>
+
+<?php if(isset($lists) && !empty($lists)): ?>
+<?php foreach($lists as $list): ?>
+<div>
+    <img src="<?php echo $list['path']; ?>" />
+</div>
+<?php endforeach; ?>
+<?php else: ?>
+<div style='color: red; font-weight: bolder; font-size: 14'>
+    未匹配到图片
+</div>
+<?php endif; ?>
+
+<?php endif; ?>
